a89bd7431be76502c3a3491ab7de585dfa5cc070
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-02-23 08:46:11 +0100

    Update SA_deployment_pipeline.ps1

--- SA_deployment_pipeline.ps1
+++ SA_deployment_pipeline.ps1
@@ -12,7 +12,7 @@ $SPREADSHEET_ID_ab = '1KPakZyyuyHoRO5GCdyde-vOvKq2155pTl-VZKKIKcXI'
 $SPREADSHEET_ID_loc = '1BZ6zKNwglzz8e3qhx1YCOMYxiSSnbDvRb3YxF-vvf_4'
 $JSON_FILENAME = $output_path_1
 $source_file_name = $source_file_name + "_ABtesting"
-$CONFIG_ab = "C:\Users\fagio\Documents\parenttext-deployment\parenttext-" + $deployment + "-repo\edits\ab_config.json"
+$CONFIG_ab = "C:\Users\fagio\Documents\parenttext-deployment\parenttext-" + $deployment + "-repo\edits\ab_config_demo.json"
 $output_path_2 = "C:\Users\fagio\Documents\parenttext-deployment\parenttext-" + $deployment + "-repo\temp\" + $source_file_name + ".json"
 Set-Location "C:\Users\fagio\Documents\rapidpro_abtesting"
 python .\main.py $JSON_FILENAME $output_path_2 $SPREADSHEET_ID_ab $SPREADSHEET_ID_loc --format google_sheets --logfile main_AB.log --config=$CONFIG_ab
@@ -33,8 +33,8 @@ Set-Location "C:\Users\fagio\Documents\parenttext-deployment"
 
 #step 4T: add translation and add quick replies to message text
 
-$languages = @("tsn") # @("afr","sot","tsn","xho","zul")
-$2languages = @("tn") #@("af","st","tn","xh","zu")
+$languages =  @("afr","sot","tsn","xho","zul")
+$2languages = @("af","st","tn","xh","zu")
 $deployment_ = "south_africa"
 
 $input_path_T = $output_path_3
@@ -78,18 +78,19 @@ $JSON11 = "11_fix_arg_qr_translation"
 $JSON11Path = $OutputDir + '\' + $JSON11 + '.json'
 $LOG12 = "12 - Log of changes after fix_arg_qr_translation"
 $LOG13 = "13 - Log of erros in file found using overall_integrity_check"
+$LOG14 = $OutputDir + "\Excel Acceptance Log.xlsx"
     
 Node C:\Users\fagio\Documents\GitHub\idems_translation\chatbot\index.js has_any_words_check $InputFile $OutputDir $JSON9 $LOG10
 Node C:\Users\fagio\Documents\GitHub\idems_translation\chatbot\index.js fix_arg_qr_translation $JSON9Path $OutputDir $JSON11 $LOG12
-Node C:\Users\fagio\Documents\GitHub\idems_translation\chatbot\index.js overall_integrity_check $JSON11Path $OutputDir $LOG13
+Node C:\Users\fagio\Documents\GitHub\idems_translation\chatbot\index.js overall_integrity_check $JSON11Path $OutputDir $LOG13 $LOG14
 
 Write-Output "Completed integrity check"
 
 
-<#
-# step 4: add quick replies to message text and translation
 
-$input_path_4 = $transl_output_folder + "\" + $output_name_T +".json"
+# step 4: add quick replies to message text and translation
+$input_path_4 = $JSON11Path 
+#$input_path_4 = $transl_output_folder + "\" + $output_name_T +".json"
 $source_file_name = $source_file_name + "_no_QR"
 $select_phrases_file = "C:\Users\fagio\Documents\parenttext-deployment\parenttext-" + $deployment + "-repo\edits\select_phrases.json"
 $output_path_4 = "C:\Users\fagio\Documents\parenttext-deployment\parenttext-" + $deployment + "-repo\temp\"
@@ -101,12 +102,11 @@ Write-Output "removed quick replies"
 $input_path_5 = $output_path_4 + $output_name_4 +".json"
 $source_file_name = $source_file_name + "_safeguarding"
 $output_path_5 = "C:\Users\fagio\Documents\parenttext-deployment\parenttext-" + $deployment + "-repo\temp\"+ $source_file_name +".json"
-$safeguarding_path = "C:\Users\fagio\Documents\safeguarding-rapidpro\input\safeguarding_malaysia.json"
-node ..\safeguarding-rapidpro\add_safeguarding_to_flows.js $input_path_5 $safeguarding_path $output_path_5 "eng"
+$safeguarding_path = "C:\Users\fagio\Documents\parenttext-deployment\parenttext-" + $deployment + "-repo\edits\" + $deployment_ + "_safeguarding.json"
+node ..\Github\safeguarding-rapidpro\add_safeguarding_to_flows_mult_lang.js $input_path_5 $safeguarding_path $output_path_5
 Write-Output "added safeguarding"
 
 # step final: split in 2 json files because it's too heavy to load (need to replace wrong flow names)
 $input_path_6 = $output_path_5
-node .\idems-chatbot-repo\scripts\split_in_multiple_json_files.js $input_path_6
+node .\idems-chatbot-repo\scripts\split_in_multiple_json_files.js $input_path_6 2
 
-#>
