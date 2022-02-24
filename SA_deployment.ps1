162fe420c235ea6eb2c1126f59f036398dfc77eb
Ateamate <50666731+Ateamate@users.noreply.github.com>
2022-02-24 12:54:42 +0300

    created pipeline template

---
+++ SA_deployment.ps1
@@ -0,0 +1,12 @@
+$source_file_name = "SA-plh-international-flavour"
+$expiration_time = 60
+$deployment = "south-africa"
+$SPREADSHEET_ID_loc = '1BZ6zKNwglzz8e3qhx1YCOMYxiSSnbDvRb3YxF-vvf_4'
+
+$languages =  @("afr","sot","tsn","xho","zul")
+$2languages = @("af","st","tn","xh","zu")
+$deployment_ = "south_africa"
+
+$n_files = 2
+
+.\parenttext_deployment_pipeline.ps1
