14734758c207e06ba9667dd9691e1877fab65ca1
Ateamate <50666731+Ateamate@users.noreply.github.com>
2022-02-25 10:09:58 +0300

    Country deployment pipelines

---
+++ GG_deployment.ps1
@@ -0,0 +1,12 @@
+$source_file_name = "GG-plh-international-flavour"
+$expiration_time = 60
+$deployment = "malaysia"
+$SPREADSHEET_ID_loc = '1oEdmUA5W_knyji8Ps-5Tw2SkRTWLNMHNuLpgLSuWgXA'
+
+$languages =  @("msa")
+$2languages = @("ms")
+$deployment_ = "malaysia"
+
+$n_files = 2
+
+.\parenttext_deployment_pipeline.ps1
---
+++ JM_deployment.ps1
@@ -0,0 +1,12 @@
+$source_file_name = "JM-plh-international-flavour"
+$expiration_time = 60
+$deployment = "jamaica"
+$SPREADSHEET_ID_loc = '1YSHxIPfFJhf-jLzgd8P7-Y2-eIO_dc9jV7dkKBL1oXw'
+
+$languages =  @()
+$2languages = @()
+$deployment_ = "jamaica"
+
+$n_files = 2
+
+.\parenttext_deployment_pipeline.ps1
