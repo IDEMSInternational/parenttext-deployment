49abd5fdcd0d7101c5b1e90a84bba68edfbf24eb
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-03-02 17:31:07 +0100

    Create INTERNATIONAL_deployment.ps1

---
+++ INTERNATIONAL_deployment.ps1
@@ -0,0 +1,14 @@
+$source_file_name = "plh-international-flavour"
+$expiration_time = 60
+$deployment = "international"
+$SPREADSHEET_ID_loc = ''
+$CONFIG_ab_name = "ab_config"
+
+$languages =  @()
+$2languages = @()
+$deployment_ = "international"
+
+$n_files = 2
+
+.\parenttext_deployment_set_up_folders.ps1
+.\parenttext_deployment_pipeline.ps1
