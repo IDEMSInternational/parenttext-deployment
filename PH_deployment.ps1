9c4d778344e838402d72a6c0f13a286c980b57a3
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-03-11 14:12:48 +0100

    Update PH_deployment.ps1

--- PH_deployment.ps1
+++ PH_deployment.ps1
@@ -1,13 +1,14 @@
 $source_file_name = "PH-plh-international-flavour"
 $default_expiration_time = 60
 $deployment = "philippines"
-$SPREADSHEET_ID_loc = '1BZ6zKNwglzz8e3qhx1YCOMYxiSSnbDvRb3YxF-vvf_4'
+$SPREADSHEET_ID_loc = '1oEdmUA5W_knyji8Ps-5Tw2SkRTWLNMHNuLpgLSuWgXA'
 $CONFIG_ab_name = "ab_config"
 
-$languages =  "fil"
-$2languages = "fil"
+$languages =  @("fil")
+$2languages = @("fil")
 $deployment_ = "philippines"
 
 $n_files = 2
 
+.\parenttext_deployment_set_up_folders.ps1
 .\parenttext_deployment_pipeline.ps1
