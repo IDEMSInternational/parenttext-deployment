cacfe823dcdd762c673e2c854c7a239da8137e01
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-03-16 18:11:09 +0100

    renamed malaysia pipeline

---
+++ MY_deployment.ps1
@@ -0,0 +1,14 @@
+$source_file_name = "MY-plh-international-flavour"
+$default_expiration_time = 60
+$deployment = "malaysia"
+$SPREADSHEET_ID_loc = '1rdEI_HWP7B_Q-J5ib9UYzDbbE4IWPbzA3-DfnGdZ0AM'
+$CONFIG_ab_name = "ab_config_demo"
+
+$languages =  @("msa")
+$2languages = @("ms")
+$deployment_ = "malaysia"
+
+$n_files = 2
+
+.\parenttext_deployment_set_up_folders.ps1
+.\parenttext_deployment_pipeline.ps1
