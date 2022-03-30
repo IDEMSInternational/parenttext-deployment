05c8266d437623645bd20573e5775340147ac716
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-03-30 11:33:56 +0200

    updated jamaica files

--- JM_deployment.ps1
+++ JM_deployment.ps1
@@ -2,7 +2,7 @@ $source_file_name = "JM-plh-international-flavour"
 $default_expiration_time = 60
 $deployment = "jamaica"
 $SPREADSHEET_ID_loc = '1YSHxIPfFJhf-jLzgd8P7-Y2-eIO_dc9jV7dkKBL1oXw'
-$CONFIG_ab_name = "ab_config"
+$CONFIG_ab_name = "ab_config_demo"
 
 $languages =  @()
 $2languages = @()
