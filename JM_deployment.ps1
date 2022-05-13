888cf6dbeba77bffeddd66082b445651d7d20e0d
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-05-13 18:07:47 +0200

    Update JM_deployment.ps1

--- JM_deployment.ps1
+++ JM_deployment.ps1
@@ -1,5 +1,5 @@
 $depl_prefix = "JM"
-$default_expiration_time = 60
+$default_expiration_time = 5
 $deployment = "jamaica"
 $SPREADSHEET_ID_loc = '1YSHxIPfFJhf-jLzgd8P7-Y2-eIO_dc9jV7dkKBL1oXw'
 $CONFIG_ab_name = "ab_config_demo"
@@ -18,8 +18,8 @@ $n_files = 2
 
 
 # edit u-report flows
-$local_flows = ".\parenttext-jamaica-repo\flows\jamaica-u-report-flows.json"
-$edited_local_flows = ".\parenttext-jamaica-repo\temp\jamaica-u-report-flows_edited.json"
+$local_flows = ".\parenttext-jamaica-repo\flows\jamaica-development.json"
+$edited_local_flows = ".\parenttext-jamaica-repo\temp\jamaica-development_edited.json"
 
 
 $expiration_times =  ".\parenttext-jamaica-repo\edits\expiration_times_u_report.json"
@@ -31,7 +31,3 @@ $triggers =  ".\parenttext-jamaica-repo\edits\triggers.json"
 node ..\safeguarding-rapidpro\add_triggers_to_flows.js  $edited_local_flows $triggers $edited_local_flows
 Write-Output "added triggers u-report"
 
-# edit integration flows
-$integration_flows = ".\parenttext-jamaica-repo\flows\jamaica-integration-flows.json"
-$edited_integration_flows = ".\parenttext-jamaica-repo\temp\jamaica-integration-flows_edited.json"
-node .\idems-chatbot-repo\scripts\update_expiration_time.js $integration_flows $expiration_times $default_expiration_time $edited_integration_flows
