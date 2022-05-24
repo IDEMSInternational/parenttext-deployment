3a365d93eeb221b90e67e9db3a3b4a338bd2d14e
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-05-24 18:04:36 +0200

    Update split_in_multiple_json_files.js

--- idems-chatbot-repo/scripts/split_in_multiple_json_files.js
+++ idems-chatbot-repo/scripts/split_in_multiple_json_files.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+const fs = require('fs');
+const path = require('path')
 
 let input_args = process.argv.slice(2);
 
@@ -8,7 +9,7 @@ let n_files = input_args[1];
 let json_string = fs.readFileSync(input_path).toString();
 let obj_full = JSON.parse(json_string);
 
-
+let logfile = "";
 
 
 let flow_dict = {};
@@ -44,6 +45,11 @@ for (let start=0; start < n_tot_flows; start+= quot){
 for (let i=0; i < n_files; i++){
     //remove triggers if the corresponding flow is not in the batch
     remove_triggers(batches[i]);
+
+    //remove campaigns if the corresponding flow is not in the batch
+    logfile += "\n  BATCH " + (i+1)
+    remove_campaigns(batches[i]);
+
     //rename flow in enter flow nodes if the name is not up to date
     rename_flows(flow_dict,batches[i])
     let output_path = input_path.substring(0, input_path.length - 5) + "_" + (i+1) + ".json";
@@ -54,7 +60,11 @@ for (let i=0; i < n_files; i++){
     });
 }
 
+let log_file_path = path.join(path.dirname(input_path),"split_json_log.txt");
 
+fs.writeFile(log_file_path, logfile, function (err, result) {
+    if (err) console.log('error', err);
+});
 
 function remove_triggers(rapidpro_obj){
     let new_triggers = [];
@@ -66,6 +76,35 @@ function remove_triggers(rapidpro_obj){
     rapidpro_obj.triggers = new_triggers;
         
     });
+}
+
+function remove_campaigns(rapidpro_obj){
+    let new_campaigns = [];
+    rapidpro_obj.campaigns.forEach(campaign => {
+        if (campaign.events.length == 0){
+            new_campaigns.push(campaign)
+        } else {
+            let new_campaign =  JSON.parse(JSON.stringify(campaign, null, 2));
+            new_campaign.events = [];
+           for (let ev =0; ev < campaign.events.length; ev++){
+               if (campaign.events[ev].event_type == "M" || (campaign.events[ev].event_type == "F" && rapidpro_obj.flows.filter(fl => (fl.name == campaign.events[ev].flow.name)).length > 0)){
+                    new_campaign.events.push(campaign.events[ev]);
+               } else {
+                logfile += "\nfor campaign "+ campaign.name +" event removed of type: " + campaign.events[ev].event_type;
+                   if (campaign.events[ev].event_type == "F"){
+                    logfile += "\nflow not present " + campaign.events[ev].flow.name;
+                   }
+               }
+
+           }
+           if (new_campaign.events.length >0){
+            new_campaigns.push(new_campaign)
+           }
+            
+        } 
+  
+    });
+    rapidpro_obj.campaigns = new_campaigns;
 }
 
 function rename_flows(flow_dict,obj_full){
