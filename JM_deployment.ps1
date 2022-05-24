3e09f458ae0873e8c3384fce98207bbb69812b16
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-05-24 11:20:14 +0200

    Update JM_deployment.ps1

--- JM_deployment.ps1
+++ JM_deployment.ps1
@@ -1,9 +1,13 @@
 $depl_prefix = "JM"
 $default_expiration_time = 5
+$expiration_times =  ".\parenttext-jamaica-repo\edits\expiration_times_u_report.json"
 $deployment = "jamaica"
 $SPREADSHEET_ID_loc = '1YSHxIPfFJhf-jLzgd8P7-Y2-eIO_dc9jV7dkKBL1oXw'
 $CONFIG_ab_name = "ab_config_demo"
 $remove_triggers = "yes"
+$redefine_safeguarding = "yes"
+$sg_flow_uuid = "b83315a6-b25c-413a-9aa0-953bf60f223c"
+$sg_flow_name = "JM - PLH - Safeguarding - WFR interaction"
 
 $languages =  @()
 $2languages = @()
@@ -22,7 +26,7 @@ $local_flows = ".\parenttext-jamaica-repo\flows\jamaica-development.json"
 $edited_local_flows = ".\parenttext-jamaica-repo\temp\jamaica-development_edited.json"
 
 
-$expiration_times =  ".\parenttext-jamaica-repo\edits\expiration_times_u_report.json"
+
 node .\idems-chatbot-repo\scripts\update_expiration_time.js $local_flows $expiration_times $default_expiration_time $edited_local_flows
 Write-Output "updated expiration u-report"
 
