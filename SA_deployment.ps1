ca4cc1d4e760b7e352119c31f4d442c61d7736ad
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-05-24 18:05:09 +0200

    Update SA_deployment.ps1

--- SA_deployment.ps1
+++ SA_deployment.ps1
@@ -6,6 +6,9 @@ $SPREADSHEET_ID_loc = '1BZ6zKNwglzz8e3qhx1YCOMYxiSSnbDvRb3YxF-vvf_4'
 $SPREADSHEET_ID_transl = '1xQWfDC2H322glS2qcobBd5_V0iOADQ1fcYNU4MCGxTw'
 $CONFIG_ab_name = "ab_config_demo"
 
+$replace_last_interaction = $false #replace in the campaigns with last_seen_on
+$campaigns_to_remove = @() 
+
 $languages =  @("afr","sot","tsn","xho","zul")
 $2languages = @("af","st","tn","xh","zu")
 $deployment_ = "south_africa"
