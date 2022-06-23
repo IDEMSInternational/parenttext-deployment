7568dfeac47f3d51a1b98af421ffec57c1c4c4eb
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-06-24 00:12:36 +0200

    update

--- PH_deployment.ps1
+++ PH_deployment.ps1
@@ -6,13 +6,13 @@ $SPREADSHEET_ID_loc = '1oEdmUA5W_knyji8Ps-5Tw2SkRTWLNMHNuLpgLSuWgXA'
 $CONFIG_ab_name = "ab_config"
 
 $replace_last_interaction = $false #replace in the campaigns with last_seen_on
-$campaigns_to_remove = @() 
+$campaigns_to_remove = @("Remove inactive users from program") 
 
 $languages =  @("fil")
 $2languages = @("fil")
 $deployment_ = "philippines"
 
-$add_selectors = $false
+$add_selectors = "yes"
 
 $n_files = 2
 
